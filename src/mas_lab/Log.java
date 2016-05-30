ea98103b68440e7a70fe0d53c733042e172e6e45
LucaRickler <luca.rickler@gmail.com>
2016-05-30 12:51:17 +0200

    Log Formatted

--- src/mas_lab/Log.java
+++ src/mas_lab/Log.java
@@ -17,126 +17,105 @@ import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+public class Log extends OneShotBehaviour {
+	static String stringa;
 
+	TreeMap<AID, Double> restMap;
+	DFAgentDescription[] listRe;
+	boolean newFile;
+	AID sender;
 
-public class Log extends OneShotBehaviour
-{   
-        static String stringa;
-        
-        TreeMap<AID, Double> restMap;
-        DFAgentDescription[] listRe;
-        boolean newFile;
-        AID sender;
+	public Log(DFAgentDescription[] listRe, AID sender, TreeMap<AID, Double> restMap, boolean newFile) {
 
-	
-        public Log(DFAgentDescription[] listRe, AID sender, TreeMap<AID, Double> restMap, boolean newFile)
-        {
+		if (stringa == null)
+			stringa = new String();
 
-            if(stringa == null)
-                stringa = new String();
-            
-            this.listRe = listRe;
-            this.newFile = newFile;
-            this.sender = sender;
-            this.restMap = restMap;
-           		
-        }
-	
- 
-	
-	
+		this.listRe = listRe;
+		this.newFile = newFile;
+		this.sender = sender;
+		this.restMap = restMap;
+
+	}
+
 	@Override
-	public void action() 
-	{
-            if(this.listRe != null || restMap != null)
-               {     
-                    int turn = 0;// = ((Global)myAgent).getTurn();
-                    stringa = stringa.concat(String.valueOf(turn));
-                    stringa = stringa.concat(this.sender.getName());   
-			
-                    for(int i=0; i < listRe.length; i++)
-                    {
-                        	stringa = stringa.concat(",");
-				stringa = stringa.concat(String.valueOf(this.restMap.get(listRe[i].getName())));
-                    }						
-               }
-		
-                stringa = stringa.concat("\n");
-                
-                
-                if (this.newFile)
-                {
-                        //-----------------------WRITE LOG----------------------------
-                        //-----------------------WRITE LOG----------------------------
+	public void action() {
+		if (this.listRe != null || restMap != null) {
+			int turn = 0;// = ((Global)myAgent).getTurn();
+			stringa = stringa.concat(String.valueOf(turn));
+			stringa = stringa.concat(this.sender.getName());
 
-                        String aggFileName = "agg-"+String.valueOf("LOG.txt");
-                        FileWriter fstream = null;
-                	try{
-				fstream = new FileWriter(aggFileName);
+			for (int i = 0; i < listRe.length; i++) {
+				stringa = stringa.concat(",");
+				stringa = stringa.concat(String.valueOf(this.restMap.get(listRe[i].getName())));
 			}
-			catch (IOException e){
+		}
+
+		stringa = stringa.concat("\n");
+
+		if (this.newFile) {
+			// -----------------------WRITE LOG----------------------------
+			// -----------------------WRITE LOG----------------------------
+
+			String aggFileName = "agg-" + String.valueOf("LOG.txt");
+			FileWriter fstream = null;
+			try {
+				fstream = new FileWriter(aggFileName);
+			} catch (IOException e) {
 				e.printStackTrace();
-			}			
+			}
 			BufferedWriter out = new BufferedWriter(fstream);
-                        try {
+			try {
 				out.write(stringa);
 				out.flush();
 				out.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-                        try {
-                                out.close();
-                                
-                        } catch (IOException ex) {
-                               Logger.getLogger(Log.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    
-                        
-                        //-----------------------WRITE INFO----------------------------
-                        //-----------------------WRITE INFO----------------------------
+			try {
+				out.close();
 
-                        String aggFileNameInfo = "agg-"+String.valueOf("infoLOG.txt");
-                        String info = new String();
-                        for(int i=0; i<listRe.length; i++)
-			{
-                            
-                                info = info.concat(listRe[i].getName().getName());
-                        	info = info.concat(",");
-                                
-//                                Iterator services = listRe[0].getAllServices();
-//                                ServiceDescription service = null;
-//                                double rank = 0.f;
-//                                while (services.hasNext()) 
-//                                {
-//                                    service = (ServiceDescription)services.next();
-//                                    Iterator properties = service.getAllProperties();
-//                                    while (properties.hasNext()) 
-//                                    {                                        
-//                                        service.get
-//                                    }
-//                                }
+			} catch (IOException ex) {
+				Logger.getLogger(Log.class.getName()).log(Level.SEVERE, null, ex);
 			}
-                                
-		
-                        		
-                        try{
+
+			// -----------------------WRITE INFO----------------------------
+			// -----------------------WRITE INFO----------------------------
+
+			String aggFileNameInfo = "agg-" + String.valueOf("infoLOG.txt");
+			String info = new String();
+			for (int i = 0; i < listRe.length; i++) {
+
+				info = info.concat(listRe[i].getName().getName());
+				info = info.concat(",");
+
+				// Iterator services = listRe[0].getAllServices();
+				// ServiceDescription service = null;
+				// double rank = 0.f;
+				// while (services.hasNext())
+				// {
+				// service = (ServiceDescription)services.next();
+				// Iterator properties = service.getAllProperties();
+				// while (properties.hasNext())
+				// {
+				// service.get
+				// }
+				// }
+			}
+
+			try {
 				fstream = new FileWriter(aggFileNameInfo);
-			}
-			catch (IOException e){
+			} catch (IOException e) {
 				e.printStackTrace();
-			}			
-                        try {
+			}
+			try {
 				out.write(info);
 				out.flush();
 				out.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-                        
-                        
-                        stringa = null;
-                }
+
+			stringa = null;
+		}
 	}
 }
-
